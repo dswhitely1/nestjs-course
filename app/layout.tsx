026beba80ed1b4ed2d743d009c3aa4209cf74edd
Donald Whitely <>
2025-01-01 20:05:59 +0000

    Section 12-10 finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
+import type {Metadata} from "next";
+import {Inter} from 'next/font/google'
 import "@/assets/styles/global.css";
+import {APP_DESCRIPTION, APP_NAME, SERVER_URL} from "@/lib/constants";
 
-const inter = Inter({ subsets: ['latin']})
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: "ProStore",
-  description: "A modern ecommerce platform build with Next.js",
+    title: {
+        template: `%s | ${APP_NAME}`,
+        default: APP_NAME,
+    },
+    description: APP_DESCRIPTION,
+    metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${inter.className} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEST_PUBLIC_APP_NAME || 'ProStore';
+export const APP_DESCRIPTION = process.env.NEST_PUBLIC_APP_DESCRIPTION || 'A modern ecommerce platform build with Next.js';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
